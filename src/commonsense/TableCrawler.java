e9d4aa355724c899b540e1a4906ef997156bcdc6
dju90 <dju90@uw.edu>
2015-02-17 09:54:50 -0800

    Added an important comment on the free base method.

--- src/commonsense/TableCrawler.java
+++ src/commonsense/TableCrawler.java
@@ -2,11 +2,10 @@ package commonsense;
 
 import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 
-public class TableCrawler {
+public class TableCrawler { //should we make this an object, so it can handle multiple directories?
 	private static HashMap<String, HashMap<String, HashSet<Pair<String, String>>>> attMap;
 
 	public static void main(String[] args) {
@@ -14,6 +13,12 @@ public class TableCrawler {
 		crawlDir(args[0], args[1]);
 	}
 
+	/*
+	 * Crawls the directory and processes csv files 
+	 * 
+	 * @param attFileName
+	 * @param dirName
+	 */
 	private static void crawlDir(String attFileName, String dirName) {
 		AttributeFilter aFilter = new AttributeFilter(attFileName);
 
@@ -21,9 +26,8 @@ public class TableCrawler {
 		for (File f : fileDir) {
 			Integer[] relevantColumns = aFilter.relevance(f);
 			if (relevantColumns.length != 0) {
-				addToMaps(f, relevantColumns);
+				addToMap(f, relevantColumns);
 			}
-
 		}
 		// unit conversion? (google library) ...how does this affect double
 		// entity comparison later?
@@ -34,74 +38,104 @@ public class TableCrawler {
 		// attribute table?
 	}
 
-	/**
+	/*
 	 * Takes a single file and grabs data to populate the global map
 	 * 
 	 * @param scan
 	 * @param releventColumns
 	 */
-	private static void addToMaps(File f, Integer[] relevantCols) {
+	private static void addToMap(File f, Integer[] relevantCols) {
 		try {
 			Scanner scan = new Scanner(f);
-			HashMap<String, HashSet<Pair<String, String>>> tableMap = new HashMap<String, HashSet<Pair<String, String>>>();
-			Set<Set<String>> freeBaseHits = new HashSet<Set<String>>();
+			HashMap<String, HashSet<Pair<String, String>>> tableMap = null;
+			Set<Set<String>> freeBaseHits = null;
 			if (scan.hasNextLine()) {
 				String[] attributes = scan.nextLine().split(",");
-				
-				if(scan.hasNextLine()) {
+				if (scan.hasNextLine()) {
 					String[] line = scan.nextLine().split(",");
-					// check if first columns contains entities (numeric == not an entity)
+					// check if first columns contains entities (alphabetic chars)
 					int entityCol = idEntityColumn(line);
+					if (entityCol != -1) { // has a non-numeric entity column
+						tableMap = new HashMap<String, HashSet<Pair<String, String>>>();
+						freeBaseHits = new HashSet<Set<String>>();
 
-					while (scan.hasNextLine()) {
-						tableMap.put(line[entityCol], processLine(line, entityCol, attributes, relevantCols, freeBaseHits));
-						line = scan.nextLine().split(",");
+						while (scan.hasNextLine()) {
+							tableMap.put(
+									line[entityCol],
+									processLine(line, entityCol, attributes,
+											relevantCols, freeBaseHits));
+							line = scan.nextLine().split(",");
+						}
+						tableMap.put(
+								line[entityCol],
+								processLine(line, entityCol, attributes,
+										relevantCols, freeBaseHits));
 					}
-					tableMap.put(line[entityCol], processLine(line, entityCol, attributes, relevantCols, freeBaseHits));
 				}
-				
-				// key-value pair
 			}
-			attMap.put(findIntersect(freeBaseHits), tableMap);
+			if (tableMap != null && freeBaseHits != null) {
+				attMap.put(findMaxIntersect(freeBaseHits), tableMap);
+			}
 			scan.close();
-			
-		} catch(FileNotFoundException fe) {
+
+		} catch (FileNotFoundException fe) {
 			fe.printStackTrace();
 			System.exit(0);
 		}
-		
-
-
-
 	}
 
-	private static HashSet<Pair<String, String>> processLine(
-			String[] line, int entityCol, String[] attributes, Integer[] relevantCols,
+	/*
+	 * Processes a single line in a csv file.
+	 * 
+	 * @param line
+	 *            The line from the file as a String array.
+	 * @param entityCol
+	 *            The index of the column in which entity names reside.
+	 * @param attributes
+	 *            A String array of the relevant attributes in the table.
+	 * @param relevantCols
+	 *            An integer array of the relevant column indices.
+	 * @param freeBaseHits
+	 *            Pointer to collection of possible superentities.
+	 * @return The HashSet from the entity to its collection of attributes.
+	 */
+	private static HashSet<Pair<String, String>> processLine(String[] line,
+			int entityCol, String[] attributes, Integer[] relevantCols,
 			Set<Set<String>> freeBaseHits) {
-		
+
 		String entity = line[entityCol];
-		// do a free base lookup for each entity and add resulting set to file-specific map
+		// do a free base lookup for each entity and add resulting set to
+		// file-specific map
 		freeBaseHits.add(freeBaseLookup(entity));
 
 		HashSet<Pair<String, String>> attVals = new HashSet<Pair<String, String>>();
 		// grab all attributes from columns w/ index in relevantColumns
-		for(int i = 0; i < relevantCols.length; i++) {
-			attVals.add(new Pair<String,String>((String)attributes[i], (String)line[relevantCols[i]]));
+		for (int i = 0; i < relevantCols.length; i++) {
+			attVals.add(new Pair<String, String>(attributes[i], line[relevantCols[i]]));
 		}
 		return attVals;
 	}
 
-	/**
-	 * Finds the first column with non-numeric entries
+	/*
+	 * Finds the first column with useful (alphabetic) entity names
 	 * 
-	 * @return
+	 * @return The column index of the first alphabetic entry
 	 */
 	private static int idEntityColumn(String[] line) {
-		return 0;
+		int i = 0;
+		while (i < line.length) {
+			// !contains only non-alphabetic chars
+			if (!line[i].matches("[^a-zA-Z]+")) { 
+				return i;
+			}
+			i++;
+		}
+		return -1;
 	}
 
-	/**
+	/*
 	 * Looks up the set of identities that freebase ascribes to a given entity
+	 * @TODO: free base API console is DOWN
 	 * 
 	 * @return
 	 */
@@ -109,15 +143,42 @@ public class TableCrawler {
 		return null;
 	}
 
-	/**
-	 * Finds the intersection between all sets
+	/*
+	 * Finds the maximum intersection between all sets
 	 * 
 	 * @return
 	 */
-	private static String findIntersect(Set<Set<String>> freeBaseHits) {
+	private static String findMaxIntersect(Set<Set<String>> freeBaseHits) {
+		Map<String, Integer> counts = new HashMap<String, Integer>();
+		for(Set<String> set : freeBaseHits) {
+			for( String superEntity : set ) {
+				if( counts.keySet().contains(superEntity) ) {
+					counts.put(superEntity, counts.get(superEntity) + 1);
+				} else {
+					counts.put(superEntity, 1);
+				}
+			}
+		}
 		// set1.retainAll(set2)
 		// return max count? or alphanumeric?
-		return null;
+		return maxCount(counts);
+	}
+	
+	/*
+	 * Returns the string in the map with the highest count
+	 * 
+	 * @param counts
+	 * @return
+	 */
+	private static String maxCount(Map<String, Integer> counts) {
+		String maxEntity = "";
+		counts.put(maxEntity, 0);
+		for( String superEntity : counts.keySet() ) {
+			if( counts.get(superEntity) > counts.get(maxEntity) ) {
+				maxEntity = superEntity;
+			}
+		}
+		return maxEntity;
 	}
 
 }
