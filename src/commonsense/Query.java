8ce5ffec947f0748155e2059380b3e1c8ec6c6c3
TheYorkWei <york.wei@gmail.com>
2015-03-16 16:01:23 -0700

    update to query to find intersect

--- src/commonsense/Query.java
+++ src/commonsense/Query.java
@@ -1,32 +1,89 @@
 package commonsense;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 public class Query {
 	public static void main(String[] args) throws UnknownHostException {
-		if(args.length == 1) {
-		//System.out.println("starting query");
-			MongoClient mongo = new MongoClient();
+		if(args.length == 2) {
+						MongoClient mongo = new MongoClient();
 			DB db = mongo.getDB("commonsense");
 			DBCollection dbc = db.getCollection("relations"); 
-			BasicDBObject o = new BasicDBObject("entity", args[0]); 
-			//System.out.println(o);
-			//o.put("entity", args[0]);       
-			DBCursor c = dbc.find(o);
-			try {
-			while(c.hasNext()) {
-				System.out.println(c.next());
+			BasicDBObject fields = new BasicDBObject("_id", false).append("entity", false).append("type", false);
+			BasicDBObject o1 = new BasicDBObject("entity", args[0]);
+			BasicDBObject o2 = new BasicDBObject("entity", args[1]);
+			DBCursor c1 = dbc.find(o1, fields);
+			DBCursor c2 = dbc.find(o2, fields);
+			
+			List<DBObject> result1 = c1.toArray();
+			List<DBObject> result2 = c2.toArray();
+			if (result1.size() > 0 && result2.size() > 0) {
+				Map<String, Pair<Double, Double>> intersect = new HashMap<String, Pair<Double, Double>>();
+				// finds the intersection
+				for (DBObject dbo : result1) {
+					for (DBObject dbo2 : result2) {
+						for (String key : dbo.keySet()) {
+							if (dbo.get(key) != null && dbo2.get(key) != null) {
+								Pair<Double, Double> compare = new Pair<Double, Double>((Double)dbo.get(key), (Double)dbo2.get(key));
+								intersect.put(key, compare);
+							}
+						}
+					}
+				}
+				
+				if (intersect.size() > 0) {
+					determineComparison(intersect, args[0], args[1]);
+				} else {
+					System.out.println("Incomparable");
+				}
+			} else {
+				if (result1.size() == 0) {
+					System.out.println("Dont know" + args[0]);
+				} else {
+					// result2.size() == 0
+					System.out.println("Dont know" + args[1]);
+				}
 			}
-			} finally {
-				c.close();
-			}
+			c1.close();
+			c2.close();
 			mongo.close();
 		}
+	}
+
+	private static void determineComparison(Map<String, Pair<Double, Double>> intersect, String arg0, String arg1) {
+		int arg1Over2 = 0;
+		int arg1Less2 = 0;
+		int equal = 0;
+		for (Pair<Double, Double> comparison :  intersect.values() ) {
+			int res = comparison.getKey().compareTo(comparison.getValue());
+			if (res > 0) {
+				// arg1 > arg2
+				arg1Over2++;
+			} else if (res == 0) {
+				equal++;
+			} else {
+				// arg1 < arg2
+				arg1Less2++;
+			}
+		}
+		
+		if (arg1Over2 > equal && arg1Over2 > arg1Less2) {
+			System.out.println(arg0);
+		} else if ((equal >= arg1Over2 && equal >= arg1Less2) || arg1Over2 == arg1Less2) {
+			System.out.println("equal");
+		} else {
+			// arg1Less2 > equal or arg1Over2
+			System.out.println(arg1);
+		}
+		
 	}
 }
